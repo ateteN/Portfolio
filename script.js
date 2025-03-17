6525fb125fd353ef01c3b78c22c2a2ae22ca7ffb
atete <n.atete@alustudent.com>
2025-03-17 12:25:13 +0200

    update

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ window.onscroll = () => {
         header.classList.remove('sticky');
     }
 };
+window.addEventListener("scroll", () => {
+    let sections = document.querySelectorAll("section"); 
+    let navLinks = document.querySelectorAll(".navbar a"); 
+
+    let scrollY = window.scrollY + 200; 
+
+    sections.forEach((section) => {
+        let sectionTop = section.offsetTop;
+        let sectionHeight = section.offsetHeight;
+        let sectionId = section.getAttribute("id");
+
+        if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+            navLinks.forEach((link) => {
+                link.classList.remove("active");
+                if (link.getAttribute("href") === `#${sectionId}`) {
+                    link.classList.add("active");
+                }
+            });
+        }
+    });
+});
+
+const darkModeIcon = document.getElementById('darkMode-icon');
+darkModeIcon.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+});
+
+const menuIcon = document.getElementById('menu-icon');
+const navbar = document.querySelector('.navbar');
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
